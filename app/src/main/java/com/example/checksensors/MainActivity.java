697d3383a566c3e4c37223586f911f1f2ad1f898
Esesna <yarrrik2014@gmail.com>
2020-02-21 17:44:33 +0300

    Добавил гироскоп и рычажки

--- app/src/main/java/com/example/checksensors/MainActivity.java
+++ app/src/main/java/com/example/checksensors/MainActivity.java
@@ -32,16 +32,31 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity implements SensorEventListener, View.OnClickListener {
     //и вот все ниже мне комментить теперь...
     private static final String TAG = "YarikRazrabotchik";//Метка в логах,как ими пользоваться еще сам +- понял
-    boolean WriteOn;//Указывает записываются ли наши логи
-    Button btnActTwo;
-    SensorManager msensorManager;
-    TextView text_main;
-    TextView text_main2;
+    private boolean WriteOnAccel,WriteOnGyr;//Указывают какие датчики записываются в логи
+    private boolean WriteOn;//Указывает записываются ли наши логи
+    private SensorManager msensorManager;//инициализируем менеджер по управлению датчиками
+
+    //инициализируем кнопочки
+    private Button btnActTwo;
+
+    //инициализруем текстовые блоки
+    private TextView textViewAccel_main;
+    private TextView textViewGyro_main;
+    private TextView text_main2;
+
+    //момент времени с начала измерения
+    private long time_0;
+
+    //Содержимое файла для асселерометра
     private float[] accelData;
-    String LogAccel;//Содержимое файла
-    long time_0 = 0;
-    private Activity thisActivity;//так удобнее
-//лвлвллвлвв
+    private String LogAccel;
+
+    //Содержимое файла для гироскопа
+    private float[] gyrData;
+    private String LogGyr;
+
+    //так удобнее
+    private Activity thisActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,31 +74,35 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         } else {
             //если доступ не дали
         }
+
         //ну тут тонна инициализации
         accelData = new float[3];
         msensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        WriteOn = false;
-        LogAccel = "time(ms),Fx,Fy,Fz";
+        WriteOnGyr = WriteOnAccel = false;
         //запускаем главный слой
         setContentView(R.layout.activity_main);
+
         //привязываемся к элементам XML файла
         btnActTwo = findViewById(R.id.button1);
-        text_main = findViewById(R.id.textView_main);
-        text_main2 = findViewById(R.id.textView_main2);
+        textViewAccel_main = findViewById(R.id.textViewAccel_main);
+        textViewGyro_main = findViewById(R.id.textViewGyro_main);
+        text_main2 = findViewById(R.id.textViewDebug_main);
+
         //привязываем обработчик нажатий к общему интерфейсу onClick
         btnActTwo.setOnClickListener(this);
 
     }
-
+    //Обработчик нажатий
     public void onClick(View v) {
-
         //в case помещаем id наших кнопок
         switch (v.getId()) {
             case R.id.button1:
                 //-----------------------------------------------
                 if(!WriteOn){
-                    LogAccel = "time(ms),Fx,Fy,Fz";
+                    LogAccel = LogGyr = "time(ms),Fx,Fy,Fz";
                     WriteOn = true;
+
+
                     time_0 = System.currentTimeMillis();
                     btnActTwo.setText("Остановить запись");
                 }else{
@@ -109,6 +128,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onResume(){
         super.onResume();
         msensorManager.registerListener(this,msensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.SENSOR_DELAY_NORMAL);
+        msensorManager.registerListener(this,msensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),SensorManager.SENSOR_DELAY_NORMAL);
     }
     @Override
     protected void onPause(){
@@ -119,12 +139,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent event) {
         final int type = event.sensor.getType(); //Определяем тип датчика
-        if (type == Sensor.TYPE_ACCELEROMETER) { //Если акселерометр
-            accelData = event.values.clone();
-            text_main.setText("X = " + accelData[0] + "\nY = " + accelData[1] + "\nZ = " + accelData[2]);
-            if(WriteOn){
-                LogAccel += "\n" + (System.currentTimeMillis()-time_0) + "," + accelData[0] + "," + accelData[1] + "," + accelData[2];
-            }
+        switch (type){
+            case Sensor.TYPE_ACCELEROMETER:  //Если акселерометр
+                accelData = event.values.clone();
+                textViewAccel_main.setText("X = " + accelData[0] + "\nY = " + accelData[1] + "\nZ = " + accelData[2]);
+                if(WriteOnAccel){
+                    LogAccel += "\n" + (System.currentTimeMillis()-time_0) + "," + accelData[0] + "," + accelData[1] + "," + accelData[2];
+                }
+                break;
+            case Sensor.TYPE_GYROSCOPE:
+                gyrData = event.values.clone();
+                textViewGyro_main.setText("X = " + gyrData[0] + "\nY = " + gyrData[1] + "\nZ = " + gyrData[2]);
+                if(WriteOnGyr){
+                    LogGyr += "\n" + (System.currentTimeMillis()-time_0) + "," + accelData[0] + "," + accelData[1] + "," + accelData[2];
+                }
+                break;
         }
 
     }
