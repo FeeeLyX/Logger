efe2bc4c1028dc515d79c30c69a21759e05ef20c
feelyx <danik.kaplun@gmail.com>
2024-03-15 00:45:37 +0300

    update

--- app/src/main/java/com/cinnabonesapps/simplelogger/MainActivity.java
+++ app/src/main/java/com/cinnabonesapps/simplelogger/MainActivity.java
@@ -1,8 +1,7 @@
-package com.example.checksensors;
+package com.cinnabonesapps.simplelogger;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
@@ -14,21 +13,17 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
-import java.io.BufferedWriter;
+
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.text.Format;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener, View.OnClickListener {
